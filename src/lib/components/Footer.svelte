bac54668a38fb69e69e472de646c860a11307502
MedAly666 <medaly700222@gmail.com>
2025-09-18 10:34:28 +0100

    Refactor layout and add authentication page with login and signup forms

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,10 +7,10 @@
     } from "flowbite-svelte";
 </script>
 
-<Footer class="mt-12 border-t pt-6 text-sm text-gray-500">
+<Footer class="mt-auto min-mt-12 border-t pt-6 text-sm text-gray-500 ">
     <FooterCopyright href="/" by="DZ Sales" year={2025} />
     <FooterLinkGroup
-        class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400"
+        class="flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400"
     >
         <FooterLink href="/">About</FooterLink>
         <FooterLink href="/">Privacy Policy</FooterLink>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Footer from "$lib/components/Footer.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import { Button, Input, Label, TabItem, Tabs } from "flowbite-svelte";
+
+</script>
+
+<Header />
+
+<article class="h-full flex flex-col items-center justify-center p-2">
+    <Tabs tabStyle="full" contentClass="w-1/3" divider>
+        <TabItem open>
+            {#snippet titleSlot()}
+                <span class="text-bold">
+                    Login
+                </span>
+            {/snippet}
+            <form action="" method="post" class="flex flex-col gap-4">
+                <Label for="email">Email</Label>
+                <Input type="email" id="email" name="email" required />
+                <Label for="password">Password</Label>
+                <Input type="password" id="password" name="password" required />
+                <Button type="submit">Login</Button>
+            </form>
+        </TabItem>
+        <TabItem>
+            {#snippet titleSlot()}
+                <span class="text-bold">
+                    Signup
+                </span>
+            {/snippet}
+            <form action="" method="post" class="flex flex-col gap-4">
+                <Label for="fullname">Full Name</Label>
+                <Input type="text" id="fullname" name="fullname" required />
+                <Label for="email">Email</Label>
+                <Input type="email" id="email" name="email" required />
+                <Label for="password">Password</Label>
+                <Input type="password" id="password" name="password" required />
+                <Button type="submit">Signup</Button>
+            </form>
+        </TabItem>
+    </Tabs>
+</article>
+<Footer />
