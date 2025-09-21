dd9ffd5d41ed160afc7ff7035e150035760bcff8
LBahat <anwer7149@gmail.com>
2025-09-21 21:34:03 +0100

    Add Sidebar component and enhance dashboard layout; update Header for route-based rendering and improve dashboard page content

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Sidebar from "../../lib/components/Sidebar.svelte";
+
+    let { children } = $props();
+</script>
+
+<main class="w-screen h-full flex">
+    <Sidebar />
+    {@render children()}
+</main>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Dashboard</h1>
+<article class="min-h-full w-full p-2 bg-gray-100 dark:bg-gray-900">
+    <div
+        class="w-full p-4 bg-gradient-to-r from-primary-600 to-green-300 text-white shadow rounded-lg"
+    >
+        <h3 class="text-2xl font-bold">Welcome back!</h3>
+        <p class="text-lg">
+            Welcome to your dashboard! Here you can manage your account and view
+            recent activity.
+        </p>
+    </div>
+    <button class="mt-4 p-2 bg-white text-primary-600 rounded shadow"
+        >Take Action</button
+    >
+</article>
